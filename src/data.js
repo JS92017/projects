afed0bcf7b4cde10346dea1a815edd03ca30c6fb
JS <smart317@naver.com>
2025-01-10 14:53:45 +0900

    나으 프로젝트

---
+++ src/data.js
@@ -0,0 +1,25 @@
+import p1 from "./assets/birthday.png";
+import p2 from "./assets/tours.png";
+import p3 from "./assets/reviews.png";
+import p4 from "./assets/questions.png";
+
+export const projects = [
+  {
+    id: 1,
+    title: "kims-movielist",
+    url: "https://kims-movielist.netlify.app",
+    image: p1,
+  },
+  {
+    id: 2,
+    title: "myimages-kim",
+    url: "https://myimages-kim.netlify.app",
+    image: p2,
+  },
+  {
+    id: 3,
+    title: "movie-app-kims",
+    url: "https://movie-app-kims.netlify.app/",
+    image: p3,
+  },
+];
